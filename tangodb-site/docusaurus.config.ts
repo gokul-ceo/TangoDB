77851854627d683826c42143a6b2e88d232c0505
gokul-ceo <gokuljune2003@gmail.com>
2026-08-13 08:49:01 +0530

    update: updated clarity analytics

--- tangodb-site/docusaurus.config.ts
+++ tangodb-site/docusaurus.config.ts
@@ -24,6 +24,22 @@ const config: Config = {
     locales: ['en'],
   },
 
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {
+        type: 'text/javascript',
+      },
+      innerHTML: `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "y1je5lexo1");
+      `,
+    },
+  ],
+
   presets: [
     [
       'classic',
